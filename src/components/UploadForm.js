bd477509d4ee28d8c4b78a16d8108c4ff8a56e93
mayowaibi <isaac.ibidun@gmail.com>
2024-01-31 15:55:13 -0500

    Completed video upload functionality

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,24 +1,41 @@
 'use client';
 import UploadIcon from "./UploadIcon";
+import axios from "axios";
+import {useState} from "react";
 
 export default function UploadForm() {
+
+    const [isUploading, setIsUploading] = useState(false);
+
     async function upload(ev) {
         ev.preventDefault();
-        const files = ev.target?.files;
+        const files = ev.target.files;
         if (files.length > 0) {
             const file = files[0];
+            setIsUploading(true);
             const res = await axios.postForm('/api/upload', {
                 file,
-            })
+            });
+            setIsUploading(false);
+            console.log(res.data)
         }
-        console.log(res.data)
     }
 
     return (
+        <>
+        {isUploading && (
+            <div className="bg-black/90 text-white fixed inset-0 flex items-center">
+                <div className="w-full text-center">
+                    <h2 className="text-4xl mb-4">Uploading</h2>
+                    <h3 className="text-xl">Please wait...</h3>
+                </div>
+            </div>
+        )}
         <label className="bg-green-500 py-3 px-6 rounded-full inline-flex gap-2 border-2 border-blue-300/50 cursor-pointer">
-          <UploadIcon />
-          <span>Choose File</span>
-          <input onChange={upload} type="file" className="hidden"/>
+            <UploadIcon />
+            <span>Choose File</span>
+            <input onChange={upload} type="file" className="hidden"/>
         </label>
+        </>
     );
 }
